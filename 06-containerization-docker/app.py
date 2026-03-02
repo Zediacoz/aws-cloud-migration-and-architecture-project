c4071af0bb3cc2585c647dfb0c963b6aabddb6f1
dimi <dimitar.chobanovv@gmail.com>
2026-03-02 15:40:36 +0000

    Add /metrics endpoint for Prometheus

--- 06-containerization-docker/app.py
+++ 06-containerization-docker/app.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, Response
+from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,6 +10,10 @@ def hello():
 @app.route("/healthz")
 def health():
     return "ok"
+
+@app.route("/metrics")
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
